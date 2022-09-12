806ad008d238b0a39e4d8edc847a45a921ecc577
ondego12 <brianondego12@gmail.com>
2022-09-12 16:36:41 -0700

    changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,20 @@
 int main(void)
 {
 	int n;
+	int i;
 
 	for (n = 0; n < 10; n++)
 	{
+	for(i = 0; i <10; i++)
+	{
 		putchar((n%10) + '0');
-		if (n == 9)
+		putchar((i%10) + '0');
+		if (n ==9 && i ==9)
 			continue;
-		putchar(',');
-		putchar(' ');
+	putchar(',');
+	putchar(' ');
 	}
+    }	
 	putchar('\n');
-	return (0);
-}
+	return(0);
+}		
