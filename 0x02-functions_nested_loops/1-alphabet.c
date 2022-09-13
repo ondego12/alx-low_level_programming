050b41d89f780ff09f1b5cca1f5151622b016880
ondego12 <brianondego12@gmail.com>
2022-09-13 08:27:34 -0700

    aphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/*
+ * file: 1-alphabet.c
+ * Auth: Brian ondego
+ */
+
+#include "main.h"
+
+/**
+ * prints the alphabets in lowercase followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for(ch='a' ; ch<='z'; ch++)
+		_putchar(ch);
+	        _putchar('\n');
+
+}		
