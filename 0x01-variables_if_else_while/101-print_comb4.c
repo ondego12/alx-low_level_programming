80e58797226dfcf0bffa8b7095f45f3a5b5b7129
ondego12 <brianondego12@gmail.com>
2022-09-12 17:04:08 -0700

    changes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,28 +6,26 @@
  */
 int main(void)
 {
-	int n, m, l;
+	int n, i, j;
+	
 
-	for (n = 48; n < 58; n++)
+	for( n=0; n<8; n++)
 	{
-		for (m = 49; m < 58; m++)
+		for(i=n+1; i<9; i++)
 		{
-			for (l = 50; l < 58; l++)
+			for(j=i+1; j<10;j++)
 			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar((n%10) + '0');
+				putchar((i%10) + '0');
+				putchar((j%10) + '0');
+				if ( n ==7 && i ==8 && j == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
-	return (0);
+	return(0);
+
 }
