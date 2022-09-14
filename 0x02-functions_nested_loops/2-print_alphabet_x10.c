9993d2a9f68685ca83efb5b3ed780ef2f7800d2c
ondego12 <brianondego12@gmail.com>
2022-09-14 14:15:01 -0700

    commiting task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * 2-print_alphabet_x10.c - print 10 times the aphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i=0;
+
+	while(i++ <=9 )
+	{
+	for (ch = 'a' ; ch <='z'; ch++){
+		_putchar(ch);
+	}
+		_putchar('\n');
+	}
+
+
+}
+
---
+++ 0x02-functions_nested_loops/test/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - checks the code.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
