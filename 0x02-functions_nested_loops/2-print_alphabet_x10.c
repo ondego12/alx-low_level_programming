f7b6e1d8caa280212f8bd5034c2a3a34913b05e4
ondego12 <brianondego12@gmail.com>
2022-09-14 14:29:27 -0700

    changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,15 @@ void print_alphabet_x10(void)
 	char ch;
 	int i = 0;
 
-	while (i++ <= 9 )
+	while (i++ <= 9)
 	{
 
 	for (ch = 'a' ; ch <= 'z'; ch++)
-	
+
 		_putchar(ch);
-	
+
 	_putchar('\n');
-	
+
 	}
 
 
