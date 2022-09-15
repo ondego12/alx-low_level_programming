f1c85832e10b36e15b38d65d1f1c22728e3fe916
ondego12 <brianondego12@gmail.com>
2022-09-15 09:24:46 -0700

    changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,26 +8,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i+1; j < 9; j++)
 		{
-			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
+			putchar((i%10) + '0');
+			putchar((j%10) + '0');
+			
+			if ( i ==9 && j == 8)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return(0);
 }
